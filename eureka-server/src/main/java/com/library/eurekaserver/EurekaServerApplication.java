8b3df11c542677c4bf5f51503784593960845e98
arturto <tomeyan.artur@gmail.com>
2023-01-27 18:28:59 +0400

    Initial

--- eureka-server/src/main/java/com/library/eurekaserver/EurekaServerApplication.java
+++ eureka-server/src/main/java/com/library/eurekaserver/EurekaServerApplication.java
@@ -2,12 +2,14 @@ package com.library.eurekaserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@EnableEurekaServer
+@EnableEurekaClient
 @SpringBootApplication
 public class EurekaServerApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(EurekaServerApplication.class, args);
     }
-
 }
