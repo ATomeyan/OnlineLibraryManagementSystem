f99182b3e1f2f9af0db87e53ce338afb01f6fff5
arturto <tomeyan.artur@gmail.com>
2023-01-30 18:43:38 +0400

    Added, changed, moved and deleted

--- book-service/controller/src/main/java/com/library/BookServiceApplication.java
+++ book-service/controller/src/main/java/com/library/BookServiceApplication.java
@@ -2,12 +2,14 @@ package com.library;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @author Artur Tomeyan
  * @date ${DATE}
  */
 @SpringBootApplication
+@EnableEurekaClient
 public class BookServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(BookServiceApplication.class, args);
