d2d9c8d45265dd0b45b24682d6366560ab6e9f93
arturto <tomeyan.artur@gmail.com>
2023-01-31 17:32:08 +0400

    Added api gateway

---
+++ api-gateway/src/main/java/com/library/apigateway/config/CloudGatewayConfig.java
@@ -0,0 +1,28 @@
+package com.library.apigateway.config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author Artur Tomeyan
+ * @date 31/01/2023
+ */
+@Configuration
+public class CloudGatewayConfig {
+
+    @Bean
+    public RouteLocator gatewayRoutes(RouteLocatorBuilder routeLocatorBuilder) {
+        return routeLocatorBuilder.routes()
+                .route("book-service", r -> r.path("/api/v1/books")
+                        .uri("lb://book-service"))
+                .route("book-service", r -> r.path("/api/v1/authors")
+                        .uri("lb://book-service"))
+                .route("book-service", r -> r.path("/api/v1/publishers")
+                        .uri("lb://book-service"))
+                .route("book-service", r -> r.path("/api/v1/genres")
+                        .uri("lb://book-service"))
+                .build();
+    }
+}
