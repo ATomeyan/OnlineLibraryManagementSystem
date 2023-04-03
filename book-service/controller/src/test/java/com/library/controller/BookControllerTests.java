e4c0a83263275b3c05b4c1c6a52a51376475822d
Artur Tomeyan <tomeyan.artur@gmail.com>
2023-04-03 18:32:30 +0400

    Added, changed, moved and deleted

--- book-service/controller/src/test/java/com/library/controller/BookControllerTests.java
+++ book-service/controller/src/test/java/com/library/controller/BookControllerTests.java
@@ -1,21 +1,68 @@
 package com.library.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.library.dto.book.BookRequestDto;
+import com.library.entity.Book;
+import com.library.service.BookService;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+
+import static org.hamcrest.Matchers.is;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 /**
  * @author Artur Tomeyan
  * @date 31.03.2023
  */
-@SpringBootTest
+@ExtendWith(SpringExtension.class)
+@WebMvcTest(value = BookController.class)
 public class BookControllerTests {
 
     @Autowired
-    private BookController bookController;
+    private MockMvc mockMvc;
+
+    @MockBean
+    private BookService bookService;
+
+    @Autowired
+    private ObjectMapper objectMapper;
 
     @Test
-    public void addBook(){
+    public void getBooks() throws Exception {
 
+        Book book = Book.builder()
+                .title("Effective Java")
+                .page(413)
+                .publishYear(2018)
+                .edition("3th edition")
+                .description("About java best practises")
+                .build();
+
+        given(bookService.createBook(any(BookRequestDto.class)))
+                .willAnswer((invocation) -> invocation.getArgument(0));
+
+        ResultActions resultActions = mockMvc.perform(post("/api/v1/books")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(book)));
+
+        resultActions.andDo(print())
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.title", is(book.getTitle())))
+                .andExpect(jsonPath("$.page", is(book.getPage())))
+                .andExpect(jsonPath("$.publishYear", is(book.getPublishYear())))
+                .andExpect(jsonPath("$.edition", is(book.getEdition())))
+                .andExpect(jsonPath("$.description", is(book.getDescription())));
     }
 }
--- book-service/persistence/src/main/java/com/library/entity/Book.java
+++ book-service/persistence/src/main/java/com/library/entity/Book.java
@@ -14,6 +14,7 @@ import java.util.UUID;
 @Getter
 @Setter
 @ToString
+@Builder
 @Entity
 @Table(name = DBConstants.TABLE_BOOK)
 public class Book {
